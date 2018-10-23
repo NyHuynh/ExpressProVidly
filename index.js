b77f8cdeb101433ff07c0aebc622281d6c0054d5
Ny Huynh <huynhthiny.nct@gmail.com>
2018-10-23 23:15:58 +0700

    add user api

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/vidly')
     .then(()=>{console.log('Connected to MongoDB...')})
     .catch(error=>{console.error('Could not connect to MongoDB.')})
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Heroku.');
+})
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log(`Listenng port ${port}...`)});
